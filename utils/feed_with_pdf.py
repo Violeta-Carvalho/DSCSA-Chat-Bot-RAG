c2c9f9c906672e5f0c929edbe8234800c6df9122
Violeta Carvalho <violeta@tracktracerx.com>
2024-12-30 17:43:30 -0300

    EPCIS and GS1 files added

--- utils/feed_with_pdf.py
+++ utils/feed_with_pdf.py
@@ -4,15 +4,18 @@ from langchain_community.vectorstores import Chroma
 import globals as globals
 
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=300, chunk_overlap=100, length_function=len, is_separator_regex=False
-)
-
-
 def feed_with_pdfs():
     for pdf_file in globals.pdf_files:
         loader = PDFPlumberLoader(pdf_file)
         docs = loader.load_and_split()
+        chunk_size = len(docs) * 10
+        chunk_overlap = round(chunk_size / 4)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            length_function=len,
+            is_separator_regex=False,
+        )
         chunks = text_splitter.split_documents(docs)
         Chroma.from_documents(
             documents=chunks,
--- utils/globals.py
+++ utils/globals.py
@@ -2,7 +2,12 @@ from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 
 # Path and File Names
 db_path = "db"
-pdf_files = ["pdfs/dscsa.pdf", "pdfs/DSCSADispenserGuide.pdf"]
+pdf_files = [
+    "pdfs/dscsa.pdf",
+    "pdfs/DSCSADispenserGuide.pdf",
+    "pdfs/EPCIS.pdf",
+    "pdfs/GS1UserGuide.pdf",
+]
 
 # LangChaing Global Variables
 embedding = FastEmbedEmbeddings()
